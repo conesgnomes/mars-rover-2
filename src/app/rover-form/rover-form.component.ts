33f7581017d054dc04f2e478f2ea53d715df77ee
Connor Larsen and Marilyn Carlin <marilyn@>
2017-07-17 10:11:59 -0700

    build out form for users to enter date and camera information to collect photos from the mars rover, as well as a componenet to view those photos, and save them to a user account for viewing later

---
+++ src/app/rover-form/rover-form.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { MarsRoverApiPhotosService } from '../mars-rover-api-photos.service';
+import { PhotoService } from '../photo.service';
+
+@Component({
+  selector: 'app-rover-form',
+  templateUrl: './rover-form.component.html',
+  styleUrls: ['./rover-form.component.css'],
+  providers: [ MarsRoverApiPhotosService, PhotoService ]
+})
+export class RoverFormComponent {
+
+  photos: any[] = null;
+  noPhotos: boolean = false;
+  constructor(private marsRoverPhotos: MarsRoverApiPhotosService) { }
+
+  saveRoverImages(date, camera){
+    this.marsRoverPhotos.saveImages(date, camera);
+    alert('The images from ' + date + ' taken by the ' + camera + ' camera have been saved to the database.')
+  }
+
+  getRoverImages(date: string, camera: string) {
+    this.noPhotos = false;
+    this.marsRoverPhotos.getByDateAndCamera(date, camera).subscribe(response =>{
+      if(response.json().photos.length > 0)
+      {
+        this.photos = response.json();
+      }
+      else {
+        this.noPhotos = true;
+      }
+    });
+  }
+
+
+
+}
